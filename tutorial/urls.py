3c531c3ff98df8ac74e8f7046a78455f09b4ac29
Juan Barragan <juan@barragan.fr>
2023-11-02 12:30:20 +0100

    first commit

---
+++ tutorial/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from rest_framework import routers
+from tutorial.quickstart import views
+import tutorial.core.views 
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('hello/', tutorial.core.views.HelloView.as_view(), name='hello'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
